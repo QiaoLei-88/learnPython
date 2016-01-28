f96379a7b894d0a6b2bb0b89f04c16bbb48aa525
Lei Qiao <qiaol618@gmail.com>
2016-01-27 19:06:51 -0600

    update pythonChallenge/level-2 with reduce and filter

--- pythonChallenge/level-2/level-2.py
+++ pythonChallenge/level-2/level-2.py
@@ -1,4 +1,4 @@
-import urllib2
+import urllib2, operator
 user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
 headers = { 'User-Agent' : user_agent }
 req = urllib2.Request('http://www.pythonchallenge.com/pc/def/ocr.html', None, headers)
@@ -6,10 +6,7 @@ response = urllib2.urlopen(req)
 page = response.read()
 
 # Filter out characters.
-filtered = ""
-for c in page:
-	if c.isalpha():
-		filtered += c
+filtered = reduce(operator.add, filter(str.isalpha, page))
 
 # cut off leading statements and output.
 statement = "find rare characters in the mess below"
